66b2a4daf57924ece7ac8b631dfcddf77fcb20f6
MohdHasan2502 <mohdhasanofficialacc.02@gmail.com>
2025-03-07 20:42:47 +0530

    replace HeaderTwo component with updated content in Header component

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -85,6 +85,31 @@ const Header = () => {
           />
         </div>
       </div>
+      <div className="flex flex-col items-center mt-10 px-4 text-center">
+        <h3 className="text-[#3C81E3] font-bold text-md">
+          Read 2X Faster With Audeus
+        </h3>
+        <h1 className="text-[#101828] text-2xl sm:text-4xl md:text-5xl font-bold mt-4 leading-tight">
+          Read Aloud PDFs, Word Docs, Your <br />
+          <span>Text, and More with Audeus</span>
+        </h1>
+        <h4 className="text-[#475467] text-lg sm:text-md font-medium mt-4">
+          Listen to PDFs, docs, and your text with our text-to-speech app to
+          save time and boost productivity.
+        </h4>
+
+        {/* ✅ Always shows the same button */}
+        <button
+          // onClick={loginHandler}
+          className="bg-[#0069FF] text-white font-bold px-16 py-4 rounded-md mt-6 shadow-md text-2xl"
+        >
+          Get started for free today
+        </button>
+
+        <p className="text-[#8a8789] font-medium text-sm mt-2">
+          Get Started In Less Than 60 Seconds. Cancel Anytime.
+        </p>
+      </div>
     </>
   );
 };
